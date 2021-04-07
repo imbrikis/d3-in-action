30f66e38e3c3f489c607d000927da9b1ff9bac6d
imbrikis <imbrikis@yahoo.com>
2021-04-07 15:59:44 -0600

    starter for 4.5

--- Chapter 4/index.js
+++ Chapter 4/index.js
@@ -2,95 +2,102 @@ const svg = d3.select('#container')
 const width = 500
 const height = 500
 
-d3.csv('./tweetData.csv').then((data) => lineChart(data))
+d3.csv('./movies.csv').then((data) => streamgraph(data))
 
-const lineChart = (data) => {
+const streamgraph = (data) => {
   console.log(data)
-
-  const blue = '#5eaec5',
-    green = '#92c463',
-    orange = '#fe9a22'
-  const xScale = d3.scaleLinear().domain([1, 10.5]).range([20, 480])
-  const yScale = d3.scaleLinear().domain([0, 35]).range([480, 20])
-  const xAxis = d3
-    .axisBottom()
-    .scale(xScale)
-    .tickSize(480)
-    .tickValues([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-  const yAxis = d3.axisRight().scale(yScale).ticks(10).tickSize(480)
-
-  const lambdaXScale = (d) => xScale(d.day)
-
-  const lineGenerator = (yAccessor, curve) => {
-    let generatedLine = d3
-      .line()
-      .x(lambdaXScale)
-      .y((d) => yScale(d[yAccessor]))
-      .curve(curve)
-
-    return generatedLine(data)
-  }
-
-  svg
-    .append('path')
-    .attr('d', lineGenerator('tweets', d3.curveBasis))
-    .attr('fill', 'none')
-    .attr('stroke', orange)
-    .attr('stroke-width', 2)
-
-  svg
-    .append('path')
-    .attr('d', lineGenerator('retweets', d3.curveStep))
-    .attr('fill', 'none')
-    .attr('stroke', blue)
-    .attr('stroke-width', 2)
-
-  svg
-    .append('path')
-    .attr('d', lineGenerator('favorites', d3.curveCardinal))
-    .attr('fill', 'none')
-    .attr('stroke', green)
-    .attr('stroke-width', 2)
-
-  svg.append('g').attr('id', 'xAxisG').call(xAxis)
-  svg.append('g').attr('id', 'yAxisG').call(yAxis)
-
-  d3.selectAll('.tick > line').style('stroke', 'lightgray')
-  d3.selectAll('.domain').style('stroke', 'lightgray')
-
-  svg
-    .selectAll('circle.tweets')
-    .data(data)
-    .enter()
-    .append('circle')
-    .attr('class', 'tweets')
-    .attr('r', 5)
-    .attr('cx', (d) => xScale(d.day))
-    .attr('cy', (d) => yScale(d.tweets))
-    .style('fill', orange)
-
-  svg
-    .selectAll('circle.retweets')
-    .data(data)
-    .enter()
-    .append('circle')
-    .attr('class', 'retweets')
-    .attr('r', 5)
-    .attr('cx', (d) => xScale(d.day))
-    .attr('cy', (d) => yScale(d.retweets))
-    .style('fill', blue)
-
-  svg
-    .selectAll('circle.favorites')
-    .data(data)
-    .enter()
-    .append('circle')
-    .attr('class', 'favorites')
-    .attr('r', 5)
-    .attr('cx', (d) => xScale(d.day))
-    .attr('cy', (d) => yScale(d.favorites))
-    .style('fill', green)
 }
+
+// SECTION 4.4
+// d3.csv('./tweetData.csv').then((data) => lineChart(data))
+
+// const lineChart = (data) => {
+//   console.log(data)
+
+//   const blue = '#5eaec5',
+//     green = '#92c463',
+//     orange = '#fe9a22'
+//   const xScale = d3.scaleLinear().domain([1, 10.5]).range([20, 480])
+//   const yScale = d3.scaleLinear().domain([0, 35]).range([480, 20])
+//   const xAxis = d3
+//     .axisBottom()
+//     .scale(xScale)
+//     .tickSize(480)
+//     .tickValues([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+//   const yAxis = d3.axisRight().scale(yScale).ticks(10).tickSize(480)
+
+//   const lambdaXScale = (d) => xScale(d.day)
+
+//   const lineGenerator = (yAccessor, curve) => {
+//     let generatedLine = d3
+//       .line()
+//       .x(lambdaXScale)
+//       .y((d) => yScale(d[yAccessor]))
+//       .curve(curve)
+
+//     return generatedLine(data)
+//   }
+
+//   svg
+//     .append('path')
+//     .attr('d', lineGenerator('tweets', d3.curveBasis))
+//     .attr('fill', 'none')
+//     .attr('stroke', orange)
+//     .attr('stroke-width', 2)
+
+//   svg
+//     .append('path')
+//     .attr('d', lineGenerator('retweets', d3.curveStep))
+//     .attr('fill', 'none')
+//     .attr('stroke', blue)
+//     .attr('stroke-width', 2)
+
+//   svg
+//     .append('path')
+//     .attr('d', lineGenerator('favorites', d3.curveCardinal))
+//     .attr('fill', 'none')
+//     .attr('stroke', green)
+//     .attr('stroke-width', 2)
+
+//   svg.append('g').attr('id', 'xAxisG').call(xAxis)
+//   svg.append('g').attr('id', 'yAxisG').call(yAxis)
+
+//   d3.selectAll('.tick > line').style('stroke', 'lightgray')
+//   d3.selectAll('.domain').style('stroke', 'lightgray')
+
+//   svg
+//     .selectAll('circle.tweets')
+//     .data(data)
+//     .enter()
+//     .append('circle')
+//     .attr('class', 'tweets')
+//     .attr('r', 5)
+//     .attr('cx', (d) => xScale(d.day))
+//     .attr('cy', (d) => yScale(d.tweets))
+//     .style('fill', orange)
+
+//   svg
+//     .selectAll('circle.retweets')
+//     .data(data)
+//     .enter()
+//     .append('circle')
+//     .attr('class', 'retweets')
+//     .attr('r', 5)
+//     .attr('cx', (d) => xScale(d.day))
+//     .attr('cy', (d) => yScale(d.retweets))
+//     .style('fill', blue)
+
+//   svg
+//     .selectAll('circle.favorites')
+//     .data(data)
+//     .enter()
+//     .append('circle')
+//     .attr('class', 'favorites')
+//     .attr('r', 5)
+//     .attr('cx', (d) => xScale(d.day))
+//     .attr('cy', (d) => yScale(d.favorites))
+//     .style('fill', green)
+// }
 
 // Section 4.3
 // const tickSize = 470
