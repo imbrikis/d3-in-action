70bcd81156c7a4094fe6a7b605056b31b852bf88
imbrikis <imbrikis@yahoo.com>
2021-04-07 16:53:47 -0600

    added d3-legend

--- Chapter 4/index.js
+++ Chapter 4/index.js
@@ -6,6 +6,60 @@ d3.csv('./movies.csv').then((data) => streamgraph(data))
 
 const streamgraph = (data) => {
   console.log(data)
+
+  const fillScale = d3
+    .scaleOrdinal()
+    .domain(['titanic', 'avatar', 'akira', 'frozen', 'deliverance', 'avengers'])
+    .range(['#fcd88a', '#cf7c1c', '#93c464', '#75734F', '#5eafc6', '#41a368'])
+  const xScale = d3.scaleLinear().domain([1, 10]).range([20, 470])
+  const yScale = d3.scaleLinear().domain([0, 55]).range([480, 20])
+
+  const xAxis = d3
+    .axisBottom()
+    .scale(xScale)
+    .tickSize(480)
+    .tickValues([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+  const yAxis = d3.axisRight().scale(yScale).ticks(10).tickSize(480)
+
+  svg.append('g').attr('id', 'xAxisG').call(xAxis)
+  svg.append('g').attr('id', 'yAxisG').call(yAxis)
+
+  d3.selectAll('.tick > line').style('stroke', 'lightgray')
+  d3.selectAll('.domain').style('stroke', 'lightgray')
+
+  const simpleStacking = (lineData, lineKey) => {
+    var newHeight = 0
+    Object.keys(lineData).every((key) => {
+      if (key !== 'day') {
+        newHeight += parseInt(lineData[key])
+        if (key === lineKey) {
+          return false
+        }
+      }
+      return true
+    })
+    return newHeight
+  }
+
+  Object.keys(data[0]).forEach((key) => {
+    if (key != 'day') {
+      const movieArea = d3
+        .area()
+        .x((d) => xScale(d.day))
+        .y0((d) => yScale(simpleStacking(d, key) - d[key]))
+        .y1((d) => yScale(simpleStacking(d, key)))
+        .curve(d3.curveBasis)
+
+      svg
+        .append('path')
+        .attr('id', key + 'Area')
+        .attr('d', movieArea(data))
+        .attr('fill', fillScale(key))
+        .attr('stroke', 'black')
+        .attr('stroke-width', 1)
+    }
+  })
 }
 
 // SECTION 4.4
